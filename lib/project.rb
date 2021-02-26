4f0df7fe40b398c1ab4404335f7c787ef71ddd68
github username <github email address>
2021-02-25 23:59:20 -0500

    commit tree f9b9413b9396035e953b926dc7da19f490df430d

---
+++ lib/project.rb
@@ -0,0 +1,16 @@
+class Project
+    attr_accessor :backer, :title
+
+    def initialize(title)
+        @title=title
+        @backer=backer
+    end
+
+    def add_backer(backer)
+        ProjectBacker.new(self, backer)
+    end
+
+    def backers
+        ProjectBacker.all.select{|pb| pb.project == self}.map{|project| project.backer} 
+    end 
+end
